366613cf074957908501852a9c540620cb8160df
BrandonMcCoy1415 <34847710+BrandonMcCoy1415@users.noreply.github.com>
2020-11-09 05:16:33 -0500

    asst 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import countries from './public/lab_6/countries.js'
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -21,13 +21,18 @@ app.use((req, res, next) => {
 });
 
 app.route('/api')
-  .get((req, res) => {
+  .get(async(req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
+    console.log('fetch request data', data);
   })
-  .post((req, res) => {
+  .post(async(req, res) => {
     console.log('POST request detected');
-    res.json(countries);
+    console.log('Form data in res.body', req.body);
+    
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = data.json();
+    console.log('data from fetch', json);
+    res.json(json);
   });
 
 app.listen(port, () => {
